d4825f42c1ffe587dfba185be867de5c602057a4
Alan Deng <alanhaledc@gmail.com>
2022-05-14 23:03:02 +0800

    refactor: use official template code

--- typescript/type-challenges/easy/00014-easy-first.ts
+++ typescript/type-challenges/easy/00014-easy-first.ts
@@ -1,23 +1,49 @@
-/**
-Implement a generic First<T> that takes an Array T and returns it's first element's type.
+/*
+  14 - First of Array
+  -------
+  by Anthony Fu (@antfu) #easy #array
+  
+  ### Question
+  
+  Implement a generic `First<T>` that takes an Array `T` and returns it's first element's type.
+  
+  For example
+  
+  ```ts
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+  
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
+  ```
+  
+  > View on GitHub: https://tsch.js.org/14
+*/
 
-For example
+/* _____________ Your Code Here _____________ */
 
-type arr1 = ['a', 'b', 'c']
-type arr2 = [3, 2, 1]
+type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
 
-type head1 = First<arr1> // expected to be 'a'
-type head2 = First<arr2> // expected to be 3
- */
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
 
-export {};
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+];
 
-type arr1 = ["a", "b", "c"];
-type arr2 = [3, 2, 1];
+type errors = [
+  // @ts-expect-error
+  First<"notArray">,
+  // @ts-expect-error
+  First<{ 0: "arrayLike" }>
+];
 
-type head1 = First<arr1>; // expected to be 'a'
-type head2 = First<arr2>; // expected to be 3
-
-// ------------------------------------------------
-
-type First<T extends any[]> = T[0];
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/14/answer
+  > View solutions: https://tsch.js.org/14/solutions
+  > More Challenges: https://tsch.js.org
+*/
--- typescript/type-challenges/easy/00018-easy-tuple-length.ts
+++ typescript/type-challenges/easy/00018-easy-tuple-length.ts
@@ -1,29 +1,53 @@
-/**
-For given a tuple, you need create a generic Length, pick the length of the tuple
+/*
+  18 - Length of Tuple
+  -------
+  by sinoon (@sinoon) #easy #tuple
+  
+  ### Question
+  
+  For given a tuple, you need create a generic `Length`, pick the length of the tuple
+  
+  For example
+  
+  ```ts
+  type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+  type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+  
+  type teslaLength = Length<tesla>  // expected 4
+  type spaceXLength = Length<spaceX> // expected 5
+  ```
+  
+  > View on GitHub: https://tsch.js.org/18
+*/
 
-For example
+/* _____________ Your Code Here _____________ */
 
-type tesla = ['tesla', 'model 3', 'model X', 'model Y']
-type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+type Length<T extends readonly any[]> = T["length"];
 
-type teslaLength = Length<tesla>  // expected 4
-type spaceXLength = Length<spaceX> // expected 5
- */
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
 
-export {};
-
-type tesla = ["tesla", "model 3", "model X", "model Y"];
-type spaceX = [
+const tesla = ["tesla", "model 3", "model X", "model Y"] as const;
+const spaceX = [
   "FALCON 9",
   "FALCON HEAVY",
   "DRAGON",
   "STARSHIP",
-  "HUMAN SPACEFLIGHT"
+  "HUMAN SPACEFLIGHT",
+] as const;
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<"hello world">
 ];
 
-type teslaLength = Length<tesla>; // expected 4
-type spaceXLength = Length<spaceX>; // expected 5
-
-// ------------------------------------------------
-
-type Length<T extends any[]> = T["length"];
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/18/answer
+  > View solutions: https://tsch.js.org/18/solutions
+  > More Challenges: https://tsch.js.org
+*/
