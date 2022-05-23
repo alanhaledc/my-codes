06d8b6c74904a42ea08450d7a0dc0fd87e3dcd8c
Alan Deng <alanhaledc@gmail.com>
2022-05-23 18:32:42 +0800

    refactor: remove unused code

--- engineering/rollup/rollup-js/rollup.config.prod.js
+++ engineering/rollup/rollup-js/rollup.config.prod.js
@@ -2,10 +2,6 @@ const path = require("path");
 const { uglify } = require("rollup-plugin-uglify");
 const configList = require("./rollup.config");
 
-const resolveFile = function (filePath) {
-  return path.join(__dirname, "..", filePath);
-};
-
 configList.map((config, index) => {
   config.output.sourcemap = false; // default
 
--- engineering/rollup/rollup-ts/rollup.config.prod.js
+++ engineering/rollup/rollup-ts/rollup.config.prod.js
@@ -2,10 +2,6 @@ const path = require("path");
 const { uglify } = require("rollup-plugin-uglify");
 const config = require("./rollup.config");
 
-const resolveFile = function (filePath) {
-  return path.join(__dirname, "..", filePath);
-};
-
 config.plugins = [...config.plugins, uglify()];
 
 module.exports = config;
